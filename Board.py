5ab4c46ba4f05496a74c8f05bb361144ffbad64a
Coța Octavian <45451082+cotaoctavian@users.noreply.github.com>
2019-11-25 21:44:57 +0200

    GUI added

---
+++ Board.py
@@ -0,0 +1,43 @@
+import pygame
+import os
+import numpy as np
+
+white_circle = pygame.image.load(os.path.join('assets', 'white.png'))
+black_circle = pygame.image.load(os.path.join('assets', 'black.png'))
+
+
+def draw_table(surface):
+    col = 0
+    for i in range(8):
+        row = 0
+        for j in range(8):
+            if (i + j) % 2 == 0:
+                pygame.draw.rect(surface, (14, 92, 23), (row, col, 100, 100))
+            else:
+                pygame.draw.rect(surface, (8, 163, 27), (row, col, 100, 100))
+            row += 100
+        col += 100
+
+
+class Board:
+    def __init__(self):
+        self.board = np.array([
+            ['-', '-', '-', '-', '-', '-', '-', '-'],
+            ['-', '-', '-', '-', '-', '-', '-', '-'],
+            ['-', '-', '-', '-', '-', '-', '-', '-'],
+            ['-', '-', '-', 'B', 'W', '-', '-', '-'],
+            ['-', '-', '-', 'W', 'B', '-', '-', '-'],
+            ['-', '-', '-', '-', '-', '-', '-', '-'],
+            ['-', '-', '-', '-', '-', '-', '-', '-'],
+            ['-', '-', '-', '-', '-', '-', '-', '-'],
+        ])
+
+    def draw(self, surface):
+        for i in range(8):
+            for j in range(8):
+                if self.board[i, j] == 'B':
+                    surface.blit(black_circle, (j * 100 + 12.5, i * 100 + 12.5))
+                elif self.board[i, j] == 'W':
+                    surface.blit(white_circle, (j * 100 + 12.5, i * 100 + 12.5))
+
+
--- main.py
+++ main.py
@@ -2,3 +2,27 @@
 # Team: Băcăoanu Adriana-Bianca, Chiperi Andrei, Coța Ștefan-Octavian, Pătrașcu Deny
 # Group: 3A1
 
+import pygame
+import os
+import Board
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+pygame.display.init()
+
+surface = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Othello')
+
+table = Board.Board()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        surface.fill((255, 255, 255))
+
+        Board.draw_table(surface)
+        table.draw(surface)
+
+        pygame.display.flip()
